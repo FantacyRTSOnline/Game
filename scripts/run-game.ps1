3e2db72b7502e1006d6f829b550607dadaff734d
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-12-14 12:27:22 -0500

    deleting scripts

--- scripts/run-game.ps1
+++
@@ -1,7 +0,0 @@
-# A utility script for running the game and passing common cli args to it durring dev.
-# By: ThatGuyJamal
-# Date: 12/10/2023
-
-Write-Host "Running game in dev mode..."
-
-cargo watch -x 'run --features bevy/dynamic_linking'
