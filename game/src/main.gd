531ea8c0946b7545a9aa5ca1b6a16c0d1e012b60
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-12-15 11:17:33 -0500

    fix

--- game/src/main.gd
+++ game/src/main.gd
@@ -1,19 +1,19 @@
 extends Control
 
 func _on_tutorial_pressed() -> void:
-	pass # Replace with function body.
+	pass
 
 func _on_campaign_pressed() -> void:
-	pass # Replace with function body.
+	pass
 	
 func _on_multiplayer_pressed() -> void:
-	pass # Replace with function body.
+	pass
 
 func _on_single_pressed() -> void:
-	get_tree().change_scene_to_file("res://src/menus/singleplayer/menu.tscn")
+	pass
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	pass
 
 func _on_quit_pressed() -> void:
-	get_tree().quit()
+	get_tree().quit(0)
